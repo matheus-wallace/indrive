aeaf7cdc2496078b4a2410221e591bbaf8b3b288
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-08-17 17:27:19 -0300

    feat: new card component from highligths section in login

---
+++ src/components/Login/CardLogin.jsx
@@ -0,0 +1,13 @@
+import Button from '../Form/Button';
+
+const CardLogin = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <p>Description</p>
+      <Button />
+    </div>
+  );
+};
+
+export default CardLogin;
