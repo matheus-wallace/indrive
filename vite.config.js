5ebea8086281dd22881ae08c365910f1621b2371
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-08-17 09:26:10 -0300

    chore: change settings to communicate with docker

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 8000, // you can replace this port with any port
+    watch: {
+      usePolling: true,
+    },
   },
 });
