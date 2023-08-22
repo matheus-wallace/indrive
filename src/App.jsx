7b68659602f51c811e6e4cc181f9387596786f3d
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-08-22 19:06:46 -0300

    feat: import of styles and login Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import GlobalStyles from './styles/GlobalStyles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
+import Login from './components/Templates/Login';
+import styles from './styles.module.css';
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
+    <main className={styles.app}>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </main>
   );
 }
 
 export default App;
-``;
