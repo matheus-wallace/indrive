5250f14bba0c02638ae309f6c5ab1c3fe1182699
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-08-23 07:47:33 -0300

    feat: import form login and separator of page

--- src/components/Templates/Login.jsx
+++ src/components/Templates/Login.jsx
@@ -1,11 +1,14 @@
 import './Login.module.css';
 import styles from './Login.module.css';
 import Highlights from '../highlights/highlights';
+import FormLogin from '../FormLogin/FormLogin';
 
 const Login = () => {
   return (
-    <section className={styles.main__container}>
+    <section className={styles.login__container}>
       <Highlights />
+      <div className={styles.login__detail}></div>
+      <FormLogin />
     </section>
   );
 };
