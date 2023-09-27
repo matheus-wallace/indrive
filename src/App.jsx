581b0709dc58d95543d03a6f13dfd5145accaf76
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-09-26 22:50:44 -0300

    feat: using the ThemeProvider to share Theme and toogle theme function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Templates/Login';
 import styles from './styles.module.css';
+import { ThemeContext } from './context/ThemeContext';
+import { useContext } from 'react';
+
 function App() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <main className={styles.app}>
+    <main className={styles.app} data-theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
